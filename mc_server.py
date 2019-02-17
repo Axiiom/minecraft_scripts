d08dfc3936f0735acead211114af74916cb528a8
axiiom <camallanyoung@gmail.com>
2019-02-17 07:42:09 -0500

    fixed is_running

--- mc_server.py
+++ mc_server.py
@@ -135,7 +135,7 @@ class MC:
 
         if version:
             print("Version: %s" % version)
-        if str(players_online) is not "['']":
+        if str(players_online) != "['']":
             print("PLAYERS ONLINE: \"" + str(players_online) + "\"")
             players = ""
             for s in players_online:
