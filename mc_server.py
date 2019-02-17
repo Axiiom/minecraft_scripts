fe89fdcd9eadeda32ef173bf9ecda8f53923f779
axiiom <camallanyoung@gmail.com>
2019-02-17 07:32:25 -0500

    fixed is_running

--- mc_server.py
+++ mc_server.py
@@ -19,58 +19,60 @@ class MC:
     def __init__(self, container):
         self.container = container
         self.name = container.name
-        self.is_running = (container.status == "running")
+
+    def __is_running(self):
+        return self.container.status == "running" and self.status() == "healthy"
 
     # calls "message" on this servers' rcon-cli and prints the result to the console
     def __rcon_call_loud(self, message):
-        if self.is_running:
+        if self.__is_running():
             os.system("docker exec " + self.name + " rcon-cli " + message)
 
     # interactively calls "message" on this servers' rcon-cli and returns the result
     def __rcon_call_interactive(self, message):
-        if self.is_running:
+        if self.__is_running():
             return os.popen("docker exec -i" + self.name + " rcon-cli " + message).readlines()
 
     # calls "message" on this servers' rcon-cli and returns the result
     def rcon_call(self, message):
-        if self.is_running:
+        if self.__is_running():
             return os.popen("docker exec " + self.name + " rcon-cli " + message).readlines()
 
     # starts the container
     def start(self):
-        if not self.is_running:
+        if not self.__is_running():
             self.container.start()
 
     # stops the container
     def stop(self, delay):
-        if self.is_running:
+        if self.__is_running():
             self.container.stop(timeout=delay)
 
     # restarts the container
     def restart(self, delay):
-        if self.is_running:
+        if self.__is_running():
             self.container.restart(timeout=delay)
 
     # calls save on the servers' rcon-cli
     def save(self):
-        if self.is_running:
+        if self.__is_running():
             self.__rcon_call_loud("save-all")
 
     # writes "message" to the server using rcon-cli
     def say(self, message):
-        if self.is_running:
+        if self.__is_running():
             self.__rcon_call_loud("say " + message)
 
     # gets the version of the server
     def get_version(self):
-        if self.is_running:
+        if self.__is_running():
             output = os.popen("docker exec " + self.name + " mcstatus localhost status").readlines()
             version = output[0].replace("version: ", "")
             return version.strip()
 
     # gets the current player count of the server as an integer
     def get_player_count(self):
-        if self.is_running:
+        if self.__is_running():
             pc = self.rcon_call("list")
             if pc:
                 player_count = pc[0]
@@ -85,7 +87,7 @@ class MC:
 
     # gets the total player count of the server as an integer
     def get_total_players(self):
-        if self.is_running:
+        if self.__is_running():
             pc = self.rcon_call("list")
             if pc:
                 player_count = pc[0]
@@ -100,7 +102,7 @@ class MC:
 
     # returns the online players in a list of strings
     def get_online_players(self):
-        if self.is_running:
+        if self.__is_running():
             pc = self.rcon_call("list")
             if pc:
                 player_count = pc[0]
@@ -135,19 +137,12 @@ class MC:
 
     # prints and attaches to this servers' logs
     def attach_logs(self):
-        if self.is_running:
-            print(Color.END)
-            os.system("docker logs -f " + self.name)
-
-    # prints this servers' logs
-    def print_logs(self):
-        if self.is_running:
-            print(Color.END)
-            os.system("docker logs " + self.name)
+        print(Color.END)
+        os.system("docker logs -f " + self.name)
 
     # attaches to this servers' rcon-cli
     def rcon(self):
-        if self.is_running:
+        if self.__is_running():
             command = "docker exec -i " + self.name + " rcon-cli"
             print(
                 Color.END + "\nAccessing [RCON] for " + Color.CYAN + self.name + Color.END
